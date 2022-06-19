ae87ad3a430c8357fb117a10c2dbef8b60339a5b
obaid12 <obaid6968@gmail.com>
2022-06-19 14:19:42 -0500

    main work completed

---
+++ index.js
@@ -0,0 +1,16 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':               
+                    display.innerText = eval(display.innerText);
+                    break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
